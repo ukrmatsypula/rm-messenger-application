b4723a555f6d0bf254a6d16093f327733ff2ff49
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-16 21:55:14 +0300

    icluded contact-user in contact list

--- src/components/contacts/v-contact-list.vue
+++ src/components/contacts/v-contact-list.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="v-contact-list"></div>
+  <div class="v-contact-list">
+    <v-contact-user />
+  </div>
 </template>
 
 <script>
+import vContactUser from '@/components/contacts/v-contact-user'
+
 export default {
   name: 'v-contact-list',
+  components: {
+    vContactUser,
+  }
 }
 </script>
 
