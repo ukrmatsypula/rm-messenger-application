788bf28be500af0d160ae1823b0731743fd6877b
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-21 17:52:52 +0300

    added few buttons in contacts and show phone number

--- src/components/contacts/v-contact-user-info.vue
+++ src/components/contacts/v-contact-user-info.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="v-contact-user-info"></div>
+  <div class="v-contact-user-info">
+    <img src="https://picsum.photos/200/300" class="info__avatar" />
+    <div class="info__content">
+      <div class="info__name">
+        <span>{{ contact_data.phone }}</span>
+      </div>
+      <div class="info__tools">
+        <button class="start-call">Call</button>
+        <button class="start-chat">Start chat</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'v-contact-user-info',
+  data: () => ({
+    contact_data: {},
+  }),
+  computed: {
+    ...mapState(['contacts']),
+  },
+  mounted() {
+    this.contacts.find(contact => {
+      if (contact.id === this.$route.query.id) {
+        this.contact_data = contact
+      }
+    })
+  },
 }
 </script>
 
